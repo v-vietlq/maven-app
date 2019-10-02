e67c77969048a258d023929cae1cc959f8a8b534
vietawake <vietawake@gmail.com>
2019-10-02 22:07:48 +0700

    fix test

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth root@149.28.35.8:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish root@149.28.35.8:/tmp/publish
+scp -i /opt/prod ~/jenkins/deploy/publish root@149.28.35.8:/tmp/publish
 ssh -i /opt/prod root@149.28.35.8 "/tmp/publish"
