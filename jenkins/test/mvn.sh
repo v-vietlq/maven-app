6af1b142dc4c3f122e50dd59017ee5aef709e31c
vietawake <vietawake@gmail.com>
2019-10-02 21:41:19 +0700

    fix test

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Testing code ***********"
 echo "***************************"
 
-WORKSPACE=WORKSPACE=/home/vietawake/jenkins-local/jenkins_home/workspace/maven
+WORKSPACE=/home/vietawake/jenkins-local/jenkins_home/workspace/maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
