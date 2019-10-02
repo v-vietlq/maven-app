ed01fbc9bbae79c2fe036c24379fcc2b020b9281
vietawake <vietawake@gmail.com>
2019-10-02 21:02:34 +0700

    fix workspace

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Testing code ***********"
 echo "***************************"
 
-#WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=WORKSPACE=/home/vietawake/jenkins-local/jenkins_home/workspace/maven
 
-docker run --rm  -v  $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
