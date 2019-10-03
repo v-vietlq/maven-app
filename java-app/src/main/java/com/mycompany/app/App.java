50a89154b9ee722fcbcc377169cd3cf2ff078d04
vietawake <vietawake@gmail.com>
2019-10-03 15:10:09 +0700

    abc

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Quoc Tuan";
+    private final String message = "Hello Quoc Viet";
 
     public App() {}
 
